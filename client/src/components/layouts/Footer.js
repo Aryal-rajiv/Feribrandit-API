f303e59db0f4d70379f04414bcbddf81c313a58d
Aryal-rajiv <rajivary1@gmail.com>
2025-12-24 22:48:58 +0545

    frontend

---
+++ client/src/components/layouts/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h1 className="logo-left">Feribrandit Logo</h1>
+      <p>Ethical fashion is the ultimate fashion</p>
+      <h4 className='text-center'> All right reserved &copy; Feribrandit</h4>
+    </div>
+  )
+}
+
+export default Footer
